9cf3c134267d85d0b340f2d23781bbe68d4680ba
c1y308 <755381914@qq.com>
2026-03-30 17:28:15 +0800

    删除SOEM部分

---
+++ build/CMakeFiles/MYACTUA_EtherCat.dir/src/CiA402.cpp.o.d
@@ -0,0 +1,23 @@
+CMakeFiles/MYACTUA_EtherCat.dir/src/CiA402.cpp.o: \
+ /home/cat/Myactua_Ethercat/src/CiA402.cpp /usr/include/stdc-predef.h \
+ /home/cat/Myactua_Ethercat/include/myactua/CiA402.hpp \
+ /usr/include/c++/12/cstdint \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/c++config.h \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/cpu_defines.h \
+ /usr/include/c++/12/pstl/pstl_config.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdint.h /usr/include/stdint.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wchar.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-uintn.h
